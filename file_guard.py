e112c57934deb1f644f63ff3fa7aac3908a24c09
dongchengwei <dongchengwei@cambricon.com>
2023-05-08 11:49:16 +0800

    no message

--- file_guard.py
+++ file_guard.py
@@ -7,13 +7,13 @@ guard_log_file = sys.argv[2]
 if __name__ == '__main__':
     where= 0
     while True:
-        file = open(guard_log_file)
+        file = open(guard_log_file, "r")
         file.seek(where)
         for line in file.readlines():
             os.system('echo ' + line)
         where = file.tell()
         file.close()
-        status_file = open(guard_status_file)
+        status_file = open(guard_status_file, "r")
         line = status_file.readline().strip()
         status_file.close()
         if line == "success":
