427af29d01a4ffcf0bd67505db89255dcd28ab2c
dongchengwei <dongchengwei@cambricon.com>
2023-05-10 15:20:17 +0800

    est

--- run_github_ci.sh
+++ run_github_ci.sh
@@ -1,20 +1,59 @@
 # /bin/bash
 
-#if [ -f '/github_ci/file.tar.gz' ];then
-#	echo "file existed, deleted."
-#	rm /github_ci/file.tar.gz
-#fi
-#tar -czvf /github_ci/file.tar.gz *
-echo "REPO_TIME_ID" > /github_ci/REPO_TIME_ID
-echo "working" > /github_ci/REPO_TIME_ID_RESULT
-touch /github_ci/REPO_TIME_ID_LOG
-if [ ! -f "/github_ci/REPO_TIME_ID_LOG_LIST" ];then
-	echo > /github_ci/REPO_TIME_ID_LOG_LIST
+PR_string=$(echo $GITHUB_REF | grep -Eo "/[0-9]*/")
+echo $PR_string
+pr_id=(${PR_string//// })
+echo "pr_id is $pr_id"
+
+current=`date "+%Y-%m-%d %H:%M:%S"`
+timeStamp=`date -d "$current" +%s` 
+# currentTimeStamp=$((timeStamp*1000+10#`date "+%N"`/1000000))
+currentTimeStamp="0000000000001"
+echo $currentTimeStamp
+
+repo_name="mluops"
+repo_root="/github_ci/${repo_name}_ci/"
+if [ ! -d $repo_root ];then
+    mkdir $repo_root
 fi
-if [ ! -d  "/github_ci/REPO_TIME_ID_SUB_LOGS" ];then
-	mkdir /github_ci/REPO_TIME_ID_SUB_LOGS
+
+requests_path="$repo_root/requests"
+if [ ! -d $requests_path ];then
+    mkdir $requests_path
 fi
-python3 file_guard.py /github_ci/REPO_TIME_ID_RESULT /github_ci/REPO_TIME_ID_LOG &
-python3 combine_log.py /github_ci/REPO_TIME_ID_LOG /github_ci/REPO_TIME_ID_LOG_LIST /github_ci/REPO_TIME_ID_SUB_LOGS/ /github_ci/REPO_TIME_ID_RESULT &
+
+request_name="${repo_name}_${pr_id}_${currentTimeStamp}"
+
+echo "${repo_name},${pr_id},${currentTimeStamp}" > "$requests_path/${request_name}"
+
+request_root="$repo_root/$request_name/"
+sub_logs_path="$request_root/sub_logs"
+
+if [ ! -d $request_root ];then
+    mkdir $request_root
+fi
+
+if [ -f "$request_root/code.tar.gz" ];then
+	echo "file existed, deleted."
+	rm "$request_root/code.tar.gz"
+fi
+tar -czvf "$request_root/code.tar.gz" *
+
+if [ ! -d $sub_logs_path ];then
+    mkdir $sub_logs_path
+fi
+
+echo "working" > "$request_root/status"
+
+if [ ! -f  "$request_root/log" ];then
+	touch "$request_root/log"
+fi
+
+if [ ! -f "$request_root/log_list" ];then
+    touch "$request_root/log_list"
+fi
+
+python3 file_guard.py "$request_root/status" "$request_root/log" &
+python3 combine_log.py "$request_root/log" "$request_root/log_list" "$request_root/sub_logs" "$request_root/status" &
 wait
 
